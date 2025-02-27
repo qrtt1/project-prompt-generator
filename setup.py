b327737827d217293bc03e34c8b41b13f8917cc6
qrtt1 <alan.chan@tenmax.io>
2025-02-27 15:05:53 +0800

    Release 0.1.2 to fix broken cli
    Signed-off-by: qrtt1 <alan.chan@tenmax.io>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="project-prompt-generator",
-    version="0.1.1",
+    version="0.1.2",
     author="Ching Yi, Chan",
     author_email="chingyichan.tw@gmail.com",
     description="A tool to convert project files into structured markdown for LLM prompts",
