8112290f1f69d5252c9c93e1a9aa3eaaa6a45817
Ching Yi, Chan <chingyichan.tw@gmail.com>
2025-03-09 14:07:39 +0800

    Rollout v0.1.6
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="project-prompt-generator",
-    version="0.1.5",
+    version="0.1.6",
     author="Ching Yi, Chan",
     author_email="chingyichan.tw@gmail.com",
     description="A tool to convert project files into structured markdown for LLM prompts",
